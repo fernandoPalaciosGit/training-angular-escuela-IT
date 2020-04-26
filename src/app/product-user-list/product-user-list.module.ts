64a0fa4fa3bde91fcd9a38627c2edffcdc44183e
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-04-26 20:21:52 +0200

    services must be providers

--- src/app/product-user-list/product-user-list.module.ts
+++ src/app/product-user-list/product-user-list.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { ItemListComponent } from '@product-user-list/components/item-list/item-list.component';
 import { ProductComponent } from '@product-user-list/components/product/product.component';
 import { ListComponent } from '@product-user-list/components/list/list.component';
 import { ProductUserListRoutingModule } from '@product-user-list/product-user-list-routing.module';
 import { SharedModule } from '@shares/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersService } from '@product-user-list/services/users.service';
+import { ProductService } from '@product-user-list/services/product.service';
 
 @NgModule({
   declarations: [
     ItemListComponent,
     ProductComponent,
-    ListComponent,
+    ListComponent
   ],
   imports: [
     CommonModule,
@@ -20,6 +22,10 @@ import { SharedModule } from '@shares/shared.module';
     FormsModule,
     HttpClientModule,
     SharedModule
+  ],
+  providers: [
+    ProductService,
+    UsersService
   ]
 })
 export class ProductUserListModule {
