9988d994484eac8f7773882ff89ae85cfc2d3b9a
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-05-01 21:21:16 +0200

    aply material UI components to our card products and the header toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@shares/material.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
   declarations: [],
   imports: [
     MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatTooltipModule
   ],
   exports: [
     MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatTooltipModule
   ]
 })
-export class MaterialModule { }
+export class MaterialModule {
+}
